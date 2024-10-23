b2b75b97872bde948579904cb69af408f9b0671b
Viorel Calin <134365141+ViorelCalin849@users.noreply.github.com>
2024-10-23 23:28:29 +0200

    Launch Error Correction

--- smartcampusreact.client/src/main.jsx
+++ smartcampusreact.client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import Register from './Register'; // Assuming you also have a Register component
 
@@ -8,6 +8,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Router>
             <Routes>
+                <Route path="/" element={<Navigate to="/login" />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
             </Routes>
